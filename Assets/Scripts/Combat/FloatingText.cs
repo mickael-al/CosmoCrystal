a2ea649b929b7e9781b46e05b0d5e612521614a2
Albarello <>
2021-04-16 01:33:55 +0200

    ParticleSystem

---
+++ Assets/Scripts/Combat/FloatingText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FloatingText : MonoBehaviour
+{
+    [SerializeField] private Animator animator = null;
+    [SerializeField] private TextMeshProUGUI damageText = null;
+
+    void OnEnable()
+    {
+        AnimatorClipInfo[] clipInfo = animator.GetCurrentAnimatorClipInfo(0);
+        Debug.Log(clipInfo.Length);
+        Destroy(gameObject, clipInfo[0].clip.length);
+    }
+
+    public void SetText(string text)
+    {
+        damageText.text = text;
+    }
+}
---
+++ Assets/Scripts/Combat/FloatingTextControler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FloatingTextControler : MonoBehaviour
+{
+    [SerializeField] private static FloatingText popupText = null;
+    [SerializeField] private static GameObject canvas = null;
+
+    public static void CreateFloatingText(string text, Transform location)
+    {
+        FloatingText instance = Instantiate(popupText);
+        Vector2 screenPosition = Camera.main.WorldToScreenPoint(new Vector2(location.position.x + Random.Range(-.2f, .2f), location.position.y + Random.Range(-.2f, .2f)));
+
+        instance.transform.SetParent(canvas.transform, false);
+        instance.transform.position = screenPosition;
+        instance.SetText(text);
+    }
+}
