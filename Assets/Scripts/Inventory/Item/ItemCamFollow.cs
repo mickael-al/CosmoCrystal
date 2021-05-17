f9ad15f90d34d5de1de7ad6b19c9bedea8eaf124
Albarello <>
2021-05-17 15:07:19 +0200

    Inventory

---
+++ Assets/Scripts/Inventory/Item/ItemCamFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCamFollow : MonoBehaviour
+{
+    private Camera cam = null;
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        transform.LookAt(cam.transform);
+    }
+}
