35dc5481b3acee3b667f85f83e6b942b0a0824af
Albarello <>
2021-05-30 23:51:06 +0200

    Auto-committed on --_--

--- gitSend.bat
+++ gitSend.bat
@@ -1,6 +1,7 @@
 @echo off
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
 set GIT_PATH="C:\Program Files\Git\bin\git.exe"
 %GIT_PATH% add -A
-%GIT_PATH% commit -am "Auto-committed on %date% %FORMAT%"
+%GIT_PATH% commit -am "Auto-committed on %fullstamp%"
 %GIT_PATH% pull
 %GIT_PATH% push
