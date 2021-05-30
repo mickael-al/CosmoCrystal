a642bcefc497d4fb10a959094800059365e5c2d6
Albarello <>
2021-05-30 23:48:06 +0200

    Auto-committed on 30/05/2021

--- gitSend.bat
+++ gitSend.bat
@@ -1,6 +1,6 @@
 @echo off
 set GIT_PATH="C:\Program Files\Git\bin\git.exe"
 %GIT_PATH% add -A
-%GIT_PATH% commit -am "Auto-committed on %date% $now"
+%GIT_PATH% commit -am "Auto-committed on %date% %FORMAT%"
 %GIT_PATH% pull
 %GIT_PATH% push
