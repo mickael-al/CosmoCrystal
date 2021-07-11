28646f7dc6f46c9cf724be3345416e1cd1d33775
Albarello <>
2021-07-11 02:39:11 +0200

    Auto-committed on 2021-07-11_02-39-08

--- Assets/Scripts/Object/Abilite/DestructibleAbilite.cs
+++ Assets/Scripts/Object/Abilite/DestructibleAbilite.cs
@@ -1,10 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 public class DestructibleAbilite : MonoBehaviour, InteractableAbilite
 {
     [SerializeField] private GameObject prefabParticle = null;
     [SerializeField] private Vector3 offsetVec3 = Vector3.zero;
+    [SerializeField] private List<ItemSpawnPourcentage> itemsSpawn = new List<ItemSpawnPourcentage>();
     public void Interact(Character owner)
     {
+        foreach(ItemSpawnPourcentage isp in itemsSpawn)
+        {
+            isp.Spawn(transform.position+offsetVec3,Quaternion.identity);
+        }
         Destroy(Instantiate(prefabParticle,transform.position+offsetVec3,Quaternion.identity),5.0f);
         Destroy(gameObject);
     }
---
+++ Assets/Scripts/Object/ItemSpawnPourcentage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class ItemSpawnPourcentage
+{
+    public Item itemSpawn = null;
+    [Range(0.0f, 100.0f)] public float pourcentage = 100.0f;
+    public void Spawn(Vector3 pos,Quaternion quat)
+    {
+        if(Random.Range(0.0f,100.0f) < pourcentage)
+        {
+            GameObject go = GameObject.Instantiate(Resources.Load("Item/Prefab/itemBase") as GameObject,pos,quat);
+            go.GetComponent<ItemDrop>().Item = itemSpawn;
+        }
+    }
+}
